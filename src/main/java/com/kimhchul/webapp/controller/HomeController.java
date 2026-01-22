e25ce03fd01f508c3272cd4b12643d808e94612e
122891 <kimhchul@ssg.com>
2026-01-22 15:26:56 +0900

    고객주문정보 추가

--- src/main/java/com/kimhchul/webapp/controller/HomeController.java
+++ src/main/java/com/kimhchul/webapp/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.kimhchul.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${app.version:dev}")
+    private String appVersion;
+
+    @Value("${server.port:8080}")
+    private String serverPort;
+
     @GetMapping("/")
-    public String home() {
-        return "redirect:/orders";
+    public String home(Model model) {
+        model.addAttribute("appVersion", appVersion);
+        model.addAttribute("serverPort", serverPort);
+        return "index";
     }
 }
